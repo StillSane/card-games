80df459929ca474483b4648c78ba6d1e91da3443
Alexander Bell <xanderbell99@gmail.com>
2024-05-22 11:32:46 -0400

    Added functionality to end the game early based on score.

--- war-card-game.py
+++ war-card-game.py
@@ -30,4 +30,38 @@ class Deck:
       list.append(card.name)
     return str(list)
   
-print(Deck())
+class Player:
+  def __init__(self, name):
+    self.name = name
+    self.cards = []
+    self.score = 0
+
+  def update_score(self):
+    for card in self.cards:
+      self.score += card.value
+  
+  def __repr__(self):
+    self.update_score()
+    return "{name} has {cards} cards and a score of {score}.".format(name=self.name, cards=len(self.cards), score=self.score)
+
+class Game:
+  def __init__(self):
+    self.playing_area = {}
+    self.player_one = Player(input("What is your name?"))
+    self.player_two = Player("The Computer")
+    self.deck = Deck()
+    self.winner = ""
+    ready = input("Are you ready to play? (YES to begin/NO to end game)").upper
+    if ready == "YES":
+      self.play_game()
+    if ready == "NO":
+      self.end_game()
+
+  def play_game(self):
+    pass
+
+  def end_game(self):
+    self.player_one.update_score()
+    self.player_two.update_score()
+    if self.player_one.score > self.player_two.score:
+      print("{player_one_name} has beaten {player_two_name} with a score of {player_one_score} and {player_one_cards} cards. {player_two_name} had a score of {player_two_score} and {player_two_cards} cards.".format(player_one_name=self.player_one.name, player_two_name=self.player_two.name, player_one_score=self.player_one.score, player_two_score = self.player_two_score, player_one_cards = len(self.player_one.cards), player_two_cards = len(self.player_two.cards)))
